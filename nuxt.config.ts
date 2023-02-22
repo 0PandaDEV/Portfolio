e18090cb92cdc2a2a48260e4782e60177eaf01a6
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-02-22 21:50:28 +0100

    added meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: { additionalData: '@use "@universal.css" as *;' },
-      },
+  app: {
+    head: {
+      charset: "UTF-8",
+      viewport: "width=device-width, initial-scale=1.0",
+      title: "PandaDEV",
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: "description", content: "My amazing site." },
+      ],
     },
   },
 });
