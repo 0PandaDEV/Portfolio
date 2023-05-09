88a598ef3ff678fc762111798cc81bc472c0a2e9
PandaDΞV <nils.ulmann@klangkonzept.ch>
2023-05-09 10:43:18 +0200

    fixed build error

--- app.vue
+++ app.vue
@@ -9,7 +9,7 @@
 <style>
 @font-face {
   font-family: Inter;
-  src: url("./fonts/Inter.woff2") format("woff2");
+  src: url("../assets/fonts/Inter.woff2") format("woff2");
   font-display: swap;
 }
 
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="bg"></div>
   <div class="center">
     <div class="hero">
-      <!-- <img src="logo.png" alt="logo" class="mainlogo" /> -->
+      <img src="../assets/logo.png" alt="logo" class="mainlogo" />
       <p class="forword">Hi, I'm PandaDEV</p>
       <h1 class="h1">Building Minecraft plugins and websites.</h1>
       <h2 class="h2">I'm a Developer specialized in java and web development. Also, I'm doing UI/UX Design.</h2>
@@ -22,7 +22,7 @@
 <style>
 @font-face {
   font-family: Inter;
-  src: url("/fonts/Inter.woff2") format("woff2");
+  src: url("../assets/fonts/Inter.woff2") format("woff2");
   font-display: swap;
 }
 
