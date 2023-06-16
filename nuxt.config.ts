d8816b6b40ede8d6dabc3fef084f72a27d2049fc
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-06-16 10:07:43 +0200

    added vercel analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,14 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
       title: "PandaDEV",
     },
   },
   css: ["~/global.css"],
-  devtools: {
-    enabled: true
-  },
-  modules: [
-    'nuxt-simple-sitemap',
-  ],
-  sitemap: {
-    siteUrl: 'https://pandadev.tk',
-  },
-})
+  devtools: { enabled: true },
+  modules: ["nuxt-simple-sitemap"],
+  sitemap: { siteUrl: "https://pandadev.tk" },
+  plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
+});
