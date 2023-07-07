a502ff5d337cfe38feafac78ee4f1abe476a973d
PandaDEV <nils.ulmann@klangkonzept.ch>
2023-07-07 11:49:56 +0200

    added umami analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,17 @@ export default defineNuxtConfig({
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
       title: "PandaDEV",
+      script: [
+        {
+          src: "https://umami-pandadev.vercel.app/script.js",
+          async: true,
+          "data-website-id": "2eec0e1c-71e1-40d8-9a04-1a816ad392da",
+        },
+      ],
     },
   },
   css: ["~/global.css"],
   devtools: { enabled: true },
-  modules: ["nuxt-simple-sitemap"],
+  modules: ["nuxt-simple-sitemap", "@nuxtjs/robots"],
   sitemap: { siteUrl: "https://pandadev.tk" },
-  plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
 });
