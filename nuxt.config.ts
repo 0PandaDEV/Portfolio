f2aa59acb495a578738469b636b911fb86ff694b
PandaDEV <70103896+0PandaDEV@users.noreply.github.com>
2023-11-07 17:21:41 +0100

    removed umami analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,23 +3,17 @@ export default defineNuxtConfig({
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
-      title: "Vleer",
-      script: [
+      title: "PandaDEV",
+      script:
         process.env.NODE_ENV === "production"
           ? [
               {
-                src: "https://umami-pandadev.vercel.app/script.js",
-                async: true,
-                "data-website-id": "2eec0e1c-71e1-40d8-9a04-1a816ad392da",
+                src: "https://plausible.pandadev.net/js/script.js",
+                defer: true,
+                "data-domain": "pandadev.net",
               },
             ]
           : [],
-        {
-          src: "https://plausible.pandadev.net/js/script.js",
-          defer: true,
-          "data-domain": "pandadev.net",
-        },
-      ],
     },
   },
   site: {
@@ -27,7 +21,4 @@ export default defineNuxtConfig({
   },
   modules: ["nuxt-simple-sitemap", "@nuxtjs/robots"],
   devtools: { enabled: true },
-  devServer: {
-    port: 443,
-  },
 });
